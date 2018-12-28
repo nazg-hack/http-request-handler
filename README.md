abd651891e00ef6abbd74567f86a802ea228a5b6
ytake <yuuki.takezawa@comnect.jp.net>
2018-12-29 00:11:15 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# http-request-handler
-for Hack
+# HTTP Server Request Handlers
+
+PSR-15 Like HTTP Server Request Handlers Interface For Hack
+
+## Require
+
+HHVM 3.30.0 and above.  
+required [hsl-experimental](https://github.com/hhvm/hsl-experimental)
+
+## Install
+
+```bash
+$ hhvm $(which composer) require ytake/hack-http-server-request-handlers-interfaces
+```
+
+## Usage
+
+Golang style
+
+```go
+func middleware(next http.HandlerFunc) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Hello World, %s", r.RequestURI)
+		next.ServeHTTP(w, r)
+	}
+}
+```
+
+For Hack
+
+```hack
+<?hh // strict
+
+namespace Acme\Middleware;
+
+use type HH\Lib\Experimental\IO\WriteHandle;
+use type Facebook\Experimental\Http\Message\ResponseInterface;
+use type Facebook\Experimental\Http\Message\ServerRequestInterface;
+use type Nazg\Http\Server\MiddlewareInterface;
+use type Nazg\Http\Server\RequestHandlerInterface;
+
+final class ExampleMiddleware implements MiddlewareInterface {
+
+  public function process(
+    WriteHandle $writeHandle,
+    ServerRequestInterface $request,
+    RequestHandlerInterface $handler,
+  ): ResponseInterface {
+    return $handler->handle($write, $request);
+  }
+}
+```
---
+++ src/MiddlewareInterface.php
@@ -0,0 +1,45 @@
+<?hh // strict
+
+/**
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ *
+ * Copyright (c) 2018 Yuuki Takezawa
+ *
+ */
+namespace Nazg\Http\Server;
+
+use type HH\Lib\Experimental\IO\WriteHandle;
+use type Facebook\Experimental\Http\Message\ResponseInterface;
+use type Facebook\Experimental\Http\Message\ServerRequestInterface;
+
+/**
+ * An HTTP middleware component participates in processing an HTTP message,
+ * either by acting on the request or the response. This interface defines the
+ * methods required to use the middleware.
+ */
+interface MiddlewareInterface {
+
+  /**
+   * Process an incoming server request and return a response, optionally delegating
+   * response creation to a handler.
+   *
+   * @param WriteHandle $handler
+   * @param ServerRequestInterface $request
+   * @param RequestHandlerInterface $handler
+   * @return ResponseInterface
+   */
+  public function process(
+    WriteHandle $writeHandle,
+    ServerRequestInterface $request,
+    RequestHandlerInterface $handler
+  ): ResponseInterface;
+}
